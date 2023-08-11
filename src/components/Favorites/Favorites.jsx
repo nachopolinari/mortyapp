65cd89b7bec5af73449c9f327bac12643592a4ee
Nacho Polinari <nachopolinari@gmail.com>
2023-08-11 10:59:31 -0300

    creacion del filtrado

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,9 +1,23 @@
 import { connect } from 'react-redux';
 import Card from '../Card/Card';
+import { useDispatch } from 'react-redux';
+import { filterCards,orderCards } from '../../redux/action';
 
 const Favorites = ({ myFavorites }) => {
     return (
         <div>
+
+            <select name="" id="">
+                <option value="A">Ascendente</option>
+                <option value="D">Descendente</option>
+            </select>
+            <select name="" id="">
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">unknown</option>
+            </select>
+
             {
                 myFavorites?.map(
                     (
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharactersFav: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -7,7 +8,8 @@ const reducer = (state = initialState, action) => {
         case 'ADD_FAV':
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, action.payload]
+                myFavorites: [...state.allCharactersFav, action.payload],
+                allCharactersFav: [...state.allCharactersFav, action.payload]
             }
 
         case 'REMOVE_FAV':
@@ -16,6 +18,23 @@ const reducer = (state = initialState, action) => {
                 myFavorites: state.myFavorites.filter(fav => fav.id !== Number(action.payload))
             }
 
+        case 'FILTER':
+
+            const allCharactersFiltered = state.allCharactersFav.filter((char) => char.gender === action.payload);
+
+            return {
+                ...state,
+                myFavorites: state.allCharactersFiltered
+            }
+
+        case 'ORDER':
+            const allCharactersFavCopy = [...state.allCharactersFav]
+            return {
+                ...state,
+                myFavorites: action.payload === 'A'
+                    ? allCharactersFavCopy.sort((a, b) => a.id < b.id)
+                    : allCharactersFavCopy.sort((a, b) => b.id < a.id)
+            }
 
         default:
             return { ...state }
